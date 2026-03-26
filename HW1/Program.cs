d3ceaf37a644a1e5a8e7657795c1a9bcc5a6fb7e
Evgeniya <chelovektea@gmail.com>
2026-03-26 11:00:07 +0700

    HomeWork

---
+++ HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HW1
+{
+    internal class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 17;
+            int y = 12;
+            
+            Swap(ref x, ref y);
+            Console.WriteLine($"{x}, {y}");
+        }
+    }
+}
